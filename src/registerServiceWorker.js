99be05b0ef1a88b66d4e32c0623a58851af5ba32
Naseef Ameer <naseef@ecaret.com>
2022-06-13 19:36:56 +0530

    final changes

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,14 +21,14 @@ if (process.env.NODE_ENV === 'production') {
         },
         updated() {
             console.log('New content is available; please refresh.')
-            infoToaster('Please refresh', 'New content is available')
+                // infoToaster('Please refresh', 'New content is available')
         },
         offline() {
             console.log('No internet connection found. App is running in offline mode.')
         },
         error(error) {
             console.error('Error during service worker registration:', error)
-            errorToaster('Error during service worker registration', error)
+                // errorToaster('Error during service worker registration', error)
         }
     })
 }
